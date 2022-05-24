ef2cb797d19a2a0408b193c9a0a261a144b52275
eezj35 <justineezj0@gmail.com>
2022-05-24 14:11:47 +0800

    .

---
+++ jira-rendering-backend/jira-rendering-backend/src/main/java/com/example/jirarenderingbackend/JiraRenderingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jirarenderingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraRenderingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraRenderingBackendApplication.class, args);
+	}
+
+}
---
+++ jira-rendering-backend/jira-rendering-backend/src/test/java/com/example/jirarenderingbackend/JiraRenderingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jirarenderingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraRenderingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
